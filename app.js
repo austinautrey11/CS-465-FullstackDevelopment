5678547c09329f5c8a904578f08ac826dd677bfb
Austin Joel Autrey <austin@Austins-MacBook-Air.local>
2021-03-29 13:08:12 -0400

    Create new application API

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
+// trigger databse connection and mongoose schema models to be loaded
 require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+// reference new API router
 const apiRouter = require('./app_api/routes/index');
 
 var app = express();
@@ -30,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+// send requests for '/api'to the api router
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,14 +1,49 @@
-var fs = require('fs');
-
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
-
-/* GET travel view */
-const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle, trips});
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+// internal method to render the travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel',  { 
+        title: pageTitle,
+        trips: responseBody,
+        message
+    }
+    );
+};
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+    // log on the console this call over to the API
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            // call new method to handle rendering the view
+            renderTravelList(req, res, body);
+        }
+    );
 }; 
 
 
 module.exports = { 
-    travel
+    travelList
 };
